09640b671468a47ff908ddf00e1fcbc47c23d59e
deezdeeznutsnuts <trinhtiendat2311@gmail.com>
2022-11-27 09:50:52 +0700

    pushcode

---
+++ src/js/lesson1.js
@@ -0,0 +1,10 @@
+const userAge = document.querySelector('#age')
+const userHeight = document.querySelector('#height')
+
+function Click(params) {
+    const getAge = prompt('nhập tuổi của bạn');  
+
+userAge.innerText = getAge +' tuổi';
+
+}
+
